c7d58bfcb92a51b90849f2f4e5c269ea7173a844
Eugen Gustovskiy <eu.hustouski@gmail.com>
2024-06-10 20:21:29 +0200

    Improve task 1

--- src/main/java/Lecture_13/Lecture_13.java
+++ src/main/java/Lecture_13/Lecture_13.java
@@ -7,7 +7,7 @@ public class Lecture_13 {
         String[] collectionOfWords = {"молоко", "хлеб", "молоко", "йогурт", "Молоко", "Сыр", "Сметана", "мясо", "КОНФЕТЫ",
                 "кетчуп", "сыр", "творог", "Хлеб", "конфеты", "зефир", "рыба", "вода", "хрен", "помидор"};
 
-        //Lecture_13 lecture = new Lecture_13();
+        Lecture_13 lecture = new Lecture_13();
         //Задание 1.1
         //lecture.printCollectionUniqueWords(collectionOfWords);
         //Задание 1.2
@@ -23,20 +23,20 @@ public class Lecture_13 {
         //phonebook.printContacts("Смирнов");
     }
 
-    //Получаем список уникальных слов (Регистр учитываем)
+    // Получаем список уникальных слов (регистр учитываем)
     public Set<String> getCollectionUniqueWords(String[] collectionOfWords) {
         Set<String> collectionUniqueWords = new HashSet<>();
         Collections.addAll(collectionUniqueWords, collectionOfWords);
         return collectionUniqueWords;
     }
 
-    //Выводим список уникальных слов
-    public void printCollectionUniqueWords(String[] collectionUniqueWords) {
-        Set<String> uniqueWords = getCollectionUniqueWords(collectionUniqueWords);
-        System.out.println("Уникальные слова: " + uniqueWords);
+    // Выводим список уникальных слов
+    public void printCollectionUniqueWords(String[] collectionOfWords) {
+        Set<String> uniqueWords = getCollectionUniqueWords(collectionOfWords);
+        System.out.println("Уникальные слова (регистр учитывается): " + uniqueWords);
     }
 
-    //Получаем количество вхождений каждого слова (Регистр не учитываем)
+    // Получаем количество вхождений каждого слова (регистр не учитываем)
     public Map<String, Integer> getWordCount(String[] collectionOfWords) {
         Map<String, Integer> wordCount = new HashMap<>();
         for (String word : collectionOfWords) {
@@ -46,10 +46,38 @@ public class Lecture_13 {
         return wordCount;
     }
 
-    //Выводим слово и количество вхождений
+    // Выводим слово и количество вхождений
     public void printWordCount(String[] collectionOfWords) {
         Map<String, Integer> wordCount = getWordCount(collectionOfWords);
+        System.out.println("Количество вхождений каждого слова (регистр не учитывается):");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    // Получаем список уникальных слов и количество повторений каждого слова (регистр учитываем)
+    public void printUniqueAndRepeatCount(String[] arrayOfWords) {
+        List<String> unique = new ArrayList<>();
+        Map<String, Integer> repeatCount = new HashMap<>();
+        for (int i = 0; i < arrayOfWords.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arrayOfWords.length; j++) {
+                if (arrayOfWords[i].equals(arrayOfWords[j]) && i != j) {
+                    count++;
+                }
+            }
+            repeatCount.put(arrayOfWords[i], count);
+            if (count == 0) {
+                unique.add(arrayOfWords[i]);
+            }
+        }
+
+        // Выводим список уникальных слов (регистр учитываем)
+        System.out.println("Уникальные слова (регистр учитывается): " + unique);
+
+        // Выводим количество повторений каждого слова (регистр учитываем)
+        System.out.println("Количество повторений каждого слова (регистр учитывается):");
+        for (Map.Entry<String, Integer> entry : repeatCount.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
